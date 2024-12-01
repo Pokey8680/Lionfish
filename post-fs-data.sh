3061caa8e194c5136f10ebf0f0a9aa4f7fb4e049
Pokey8680 <159397396+Pokey8680@users.noreply.github.com>
2024-12-01 01:27:23 -0500

    Delete post-fs-data.sh
    Moving to customize.sh

--- post-fs-data.sh
+++
@@ -1,9 +0,0 @@
-#!/system/bin/sh
-
-# Create the ManualOTA folder in /storage/emulated/0/ if it doesn't exist
-if [ ! -d /storage/emulated/0/ManualOTA ]; then
-    mkdir /storage/emulated/0/ManualOTA
-    echo "Created ManualOTA folder in /storage/emulated/0/"
-else
-    echo "ManualOTA folder already exists."
-fi
